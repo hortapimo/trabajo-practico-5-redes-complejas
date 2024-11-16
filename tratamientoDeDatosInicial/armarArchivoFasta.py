62f7b93fa1dca9b5852f254e390ee26d01b8be56
Pedro Rabitti <63475606+hortapimo@users.noreply.github.com>
2024-11-15 23:32:38 -0300

    agregue lo del los paises

--- tratamientoDeDatosInicial/armarArchivoFasta.py
+++ tratamientoDeDatosInicial/armarArchivoFasta.py
@@ -6,16 +6,34 @@ fasta_file = "spikeprot1103.fasta"
 lista = []
 
 i=0
-limite = 400
-archivo = open("archivo.fasta","x")
+limite = 100000
+archivoAux = open("ArgentinaAhoraSi.fasta","x")
 limiteCaracteresID = 0
-# Leer el archivo FASTA
-with open(fasta_file, "r") as file:
+identifiers = {}
+
+pais="Argentina"
+
+# Leer el archivo FASTA y las secuecuencias con ID similar les agrego un numero
+with open(fasta_file, "r",encoding="latin-1") as file:
     for record in SeqIO.parse(file, "fasta"):
         if(len(str(record.id))<limiteCaracteresID):
             pass
         else:
-            archivo.write(">"+str(record.id)+"\n" +str(record.seq)+"\n")
-            i = i+1
-        if (i>limite):
-            break
+            if (record.id.split('/')[1] == pais):
+                if record.id in identifiers:
+                    # Incrementar el contador de duplicados
+                    identifiers[record.id] += 1
+
+                else:
+                     identifiers[record.id] = 0
+                     archivoAux.write(">"+str(record.id)+"\n" +str(record.seq)+"\n")
+                i = i+1
+            
+            if (i>limite):
+                break
+print(f"hubo {i} secuencias duplicadas")
+
+#Separar paor pais
+#Poner la secuencia de wohan
+#Eliminar las secuencias repetidas
+
